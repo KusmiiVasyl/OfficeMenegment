792bf5aaea95b6c217c0a453631e28fd88c8bf12
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-06-12 15:53:55 +0300

    Create form for add employee

---
+++ OfficeMenegment/Models/AddEmployeeView.cs
@@ -0,0 +1,11 @@
+﻿namespace OfficeMenegment.Models
+{
+    public class AddEmployeeView
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+        public DateTime DateBirthday { get; set; }
+    }
+}
