f5ff8af505f834c93726ce60599325816eb420f4
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-06-12 14:58:31 +0300

    Create project, create model Employee, done migration to MSSql Database

---
+++ OfficeMenegment/Data/OfficeMenegmentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfficeMenegment.Models;
+
+namespace OfficeMenegment.Data
+{
+    public class OfficeMenegmentDbContext : DbContext
+    {
+        public OfficeMenegmentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ OfficeMenegment/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace OfficeMenegment.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+        public DateTime DateBirthday { get; set; }
+    }
+}
